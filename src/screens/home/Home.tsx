227a8a341f00b4a445f7f4de779d5ec65a314447
Bruno Trindade <bruno.trindadefm@gmail.com>
2025-04-07 23:24:17 -0300

    feat: consuming pokeapi

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,10 +1,42 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, View } from "react-native";
+import axios from 'axios';
+import { useState, useEffect } from "react";
 
 const Home = () => {
+
+  const [data, setData] = useState<any[]>([])
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=20`)
+      
+      const pokemons = response.data.results;
+
+      const pokemonDetailsPromises = pokemons.map((pokemon: any) => 
+        axios.get(pokemon.url)
+      );
+      const pokemonDetailsResponses = await Promise.all(pokemonDetailsPromises);
+      const pokemonDetails = pokemonDetailsResponses.map((res) => res.data);
+      
+      setData(pokemonDetails);
+      console.log(pokemonDetails);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <View>
-      <Text>Home</Text>
+      {data.length > 0 && 
+        data.map((pokemon) => (
+          <Image key={pokemon.id} source={pokemon.sprites.other['official-artwork'].front_default || '/path/to/placeholder.png'} alt={pokemon.name} style={{height: 100, width: 100}}/>
+        ))
+      }
     </View>
   );
 };
